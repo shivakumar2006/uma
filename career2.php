50afd448db2ae9ec4ba99350860cf6aec54888f2
Vrinda-Khandelwal <vrindakhandelwal2002@gmail.com>
2026-02-13 13:23:21 +0530

    Change to Improve

--- career2.php
+++ career2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Careers | Greenwood Academy</title>
+    <title>Careers | Uma Memorial Public School</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="icon" type="image/jpg" href="images/logo-1_imresizer_1.jpg">
