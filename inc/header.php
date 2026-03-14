67d52226508ab8db4266342e27af953df1e4070d
coderbot202 <coderbotrobotech@gmail.com>
2026-03-14 11:10:57 +0530

    Integrate Google Tag Manager in header.php
    Added Google Tag Manager script for tracking.

--- inc/header.php
+++ inc/header.php
@@ -41,5 +41,15 @@
     <!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> -->
     <!-- <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"> -->
     <!-- Custom CSS for overall styling and the marquee animation -->
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11248599746"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11248599746');
+</script>
   </head>
   <body>
